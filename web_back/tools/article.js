42c1dec213672765482451bb7dd9cb6ea0a988de
杨晓磊 <1169364149@qq.com>
2019-12-17 17:42:10 +0800

    文章分类的增删改查
    文章分类的请求放在article.js中，与页面分离

---
+++ web_back/tools/article.js
@@ -0,0 +1,57 @@
+// 用来存放文章的相关操作
+
+var article = {
+    categorySearch: function (options) {
+        $.ajax({
+            url: CATEGORY_SEARCH,
+            success: function (res) {
+                // console.log(res);
+                if (res.code === 200) {
+                    options.success(res);
+                }
+            }
+        })
+    },
+    categoryAdd: function (options) {
+        $.ajax({
+            type: 'POST',
+            url: CATEGORY_ADD,
+            data: options.data,
+            success: function (res) {
+                // console.log(res);
+                if (res.code === 200) {
+                    options.success();
+                } else {
+                    options.fail();
+                }
+            }
+        })
+    },
+    categoryEdit: function (options) {
+        $.ajax({
+            type: 'POST',
+            url: CATEGORY_EDIT,
+            data: options.data,
+            success: function (res) {
+                // console.log(res);
+                if (res.code === 200) {
+                    options.success();
+                }
+            }
+        })
+    },
+    categoryDelete: function (options) {
+        $.ajax({
+            type: 'POST',
+            url: CATEGORY_DELETE,
+            data: {
+                id: options.data
+            },
+            success: function (res) {
+                if (res.code === 200) {
+                    options.success();
+                }
+            }
+        })
+    }
+}
